1319fe8349962daad5e2fc0452bcdc1f9e68f238
Everett <everett.bowline@gmail.com>
2018-02-07 13:22:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ function shout(string) {
 }
 
 function whisper(string) {
-  return `string.toLowerCase()`
+  return string.toLowerCase
 }
 
 function logShout(string) {
-  console.log ("string.toUpperCase()")
+  console.log (string.toUpperCase)
 }
 
 function logWhisper(string) {
-  console.log ("string.toLowerCase()")
+  console.log (string.toLowerCase)
 }
 
 function sayHiToGrandma(string) {
